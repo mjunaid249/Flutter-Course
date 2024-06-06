b63a155f7b6ef55c5cc96c49bf1ff51c6c787db8
mjunaid249 <junaidch249249@gmail.com>
2024-06-06 18:17:43 +0500

    statless widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.blueGrey.shade300,
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        title: const Center(
-          child: Text('Flutter'),
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey.shade300,
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: const Center(
+            child: Text('Flutter'),
+          ),
+        ),
+        body: const Center(
+          child: Image(image: AssetImage('../assets/image1.jpeg')),
         ),
       ),
-      body: const Center(
-        child: Image(image: AssetImage('../assets/image1.jpeg')),
-      ),
-    ),
-  ));
+    );
+  }
 }
